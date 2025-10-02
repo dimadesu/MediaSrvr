ca5896c1ad3e8d31737fd21a8f3fa200ae736e41
dimadesu <dimadesu@gmail.com>
2025-10-02 15:36:44 +1000

    Add "How to use" activity

---
+++ app/src/main/java/com/dimadesu/mediasrvr/HowToUseActivity.kt
@@ -0,0 +1,11 @@
+package com.dimadesu.mediasrvr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HowToUseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_to_use)
+    }
+}
